c6db96e3c2202a8c0cb3342e7110165284724edf
L1ndemberg <Juniorlindemberg21@gmail.com>
2022-10-19 21:12:21 -0300

    botao

---
+++ Javascript/contador.js
@@ -0,0 +1,55 @@
+const contador = document.getElementById("contar");
+const sumar = document.getElementById("incr");
+const restar = document.getElementById("decr");
+const reset = document.getElementById("reset");
+
+let numero = 0;
+
+sumar.addEventListener("click", ()=>{
+    numero++;
+    contador.innerHTML = numero;
+});
+
+restar.addEventListener("click", ()=>{
+
+    if(numero==0){}
+    else{
+        numero--;
+        contador.innerHTML = numero;
+    }
+
+});
+
+reset.addEventListener("click", ()=>{
+    numero = 0;
+    contador.innerHTML = numero;
+});
+
+const contador2 = document.getElementById("contar2");
+const sumar2 = document.getElementById("incr2");
+const restar2 = document.getElementById("decr2");
+const reset2 = document.getElementById("reset2");
+
+let numero2 = 0;
+
+sumar2.addEventListener("click", ()=>{
+    numero2++;
+    contador2.innerHTML = numero2;
+});
+
+restar2.addEventListener("click", ()=>{
+
+    if(numero2==0){}
+    else{
+        numero2--;
+        contador2.innerHTML = numero2;
+    }
+
+});
+
+reset.addEventListener("click", ()=>{
+    numero = 0;
+    contador.innerHTML = numero;
+});
+
+
